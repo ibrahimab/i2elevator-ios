75c6ac8f7df5ecdf9e8465589facf827cc17c892
János Kukoda <kukodajanos@icloud.com>
2024-02-19 20:07:52 +0100

    -

---
+++ i2Elevator/i2ElevatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  i2ElevatorApp.swift
+//  i2Elevator
+//
+//  Created by János Kukoda on 19/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct i2ElevatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
